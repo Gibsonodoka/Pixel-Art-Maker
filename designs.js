4f3bcd54cabafac3a33fbaa3aecfdc4b8b0a01f6
Gibson Uwhetu <100386202+Gibsonodoka@users.noreply.github.com>
2022-04-28 04:56:48 +0100

    Updated Comment

--- designs.js
+++ designs.js
@@ -3,7 +3,7 @@ const sizePicker = document.getElementById("sizePicker");
 let inputHeight = document.getElementById("inputHeight");
 let inputWidth = document.getElementById("inputWidth");
 
-// color picker
+// Create color picker
 let color = document.getElementById("colorPicker");
 
 const pixelCanvas = document.getElementById("pixelCanvas");
@@ -15,39 +15,39 @@ function clearGrid()
      row.remove();
      });
 }
-
+// create grid 
 function makeGrid(pam) {
-  // Your code goes here!
+  
   pam.preventDefault();
 
-  // ResetUSER  grid 
+  // clear  grid function, to clear the grid everytime! 
   clearGrid();
 
-  
+  // height of grid
   const height = inputHeight.value;
-  
+  // width of grid
   const width = inputWidth.value;
-
+ // Grid increment based on what theuser selects as width and height (Nested loop)
   for (let i = 1; i <= height; i++) {
-  
+  //create row element
     const row = document.createElement("tr");
     
     for (let j = 1; j <= width; j++) {
-     
+     //Column element is created
       const column = document.createElement("td");
       
       column.id = "column-i-j";
-   
+   // Append column to row element
       row.appendChild(column);
     }
     
    
-    
+    //Append row element to table element
     pixelCanvas.appendChild(row);
   }
 }
 
-//  call makeGrid() function
+//  call makeGrid() function when user clicks on submit
 
 sizePicker.addEventListener("submit", makeGrid);
 
